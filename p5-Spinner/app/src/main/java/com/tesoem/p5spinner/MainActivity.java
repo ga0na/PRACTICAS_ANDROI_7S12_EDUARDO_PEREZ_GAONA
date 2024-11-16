7344687c3c734c1cbd292fe539d45112bd9084a6
eduardo-gaona <eduardo17111084gaona@gmail.com>
2024-11-15 18:11:01 -0600

    practicas

---
+++ p5-Spinner/app/src/main/java/com/tesoem/p5spinner/MainActivity.java
@@ -0,0 +1,40 @@
+package com.tesoem.p5spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner splista;
+    List<String> Milista = new ArrayList<>(Arrays.asList("Estado de Mexico", "CDMX", "Puebla", "Morelos", "Veracruz", "Guerrero"));
+    ArrayAdapter<String> adaptador;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        splista = findViewById(R.id.apA1Estado);
+        splista.setOnItemSelectedListener((new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String valor = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this, "Se selecciono: "+ valor, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        }));
+        adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Milista);
+        splista.setAdapter(adaptador);
+    }
+}
---
+++ pfragments/app/src/main/java/com/el_toromax/pfragments/MainActivity.java
@@ -0,0 +1,40 @@
+package com.el_toromax.pfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btnF1,btnF2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnF1=findViewById(R.id.btnA1Frg1);
+        btnF2=findViewById(R.id.btnA1Frg2);
+
+        Fragment funo= new primerFragment();
+        Fragment fdos= new segundoFragment();
+
+        btnF1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction f1= getSupportFragmentManager().beginTransaction();
+                f1.replace(R.id.contenedorFragment,funo).commit();
+
+            }
+        });
+
+        btnF2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction f2= getSupportFragmentManager().beginTransaction();
+                f2.replace(R.id.contenedorFragment,fdos).commit();
+            }
+        });
+    }
+}
